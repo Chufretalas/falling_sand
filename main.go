4bf511b84d909364e5c47331dd1fc5a360056db2
Chufretalas <marcoantonio.bene@gmail.com>
2024-02-05 19:50:13 -0300

    made the canvas painteable

--- main.go
+++ main.go
@@ -14,15 +14,60 @@ import (
 	_ "github.com/silbinarywolf/preferdiscretegpu"
 )
 
-var ()
+type Game struct{}
+
+type Position struct {
+	x, y float32
+}
+
+type BlockType int
+
+const (
+	BTAIR BlockType = iota
+	BTSAND
+)
+
+type Block struct {
+	bType BlockType
+	pos   Position
+}
+
+type BlockGrid [][]Block
+
+var (
+	blocks      BlockGrid
+	blocksCopy  BlockGrid
+	activeBlock Position
+)
 
 const (
 	SCREENWIDTH  = 1920
 	SCREENHEIGHT = 1080
-	SQUARESIDE   = 10
+	SQUARESIDE   = 20
 )
 
-type Game struct{}
+func init() {
+	blocks = make([][]Block, SCREENHEIGHT/SQUARESIDE)
+	blocksCopy = make([][]Block, SCREENHEIGHT/SQUARESIDE)
+	for idx := range blocks {
+		blocks[idx] = make([]Block, SCREENWIDTH/SQUARESIDE)
+		blocksCopy[idx] = make([]Block, SCREENWIDTH/SQUARESIDE)
+	}
+
+	activeBlock = Position{0, 0}
+}
+
+func blockPos2BlocksIdx(p Position) (int, int) {
+	return int(p.y / SQUARESIDE), int(p.x / SQUARESIDE)
+}
+
+func clearBlocks() {
+	for idx1 := range blocks {
+		for idx2 := range blocks[idx1] {
+			blocks[idx1][idx2] = Block{}
+		}
+	}
+}
 
 func (g *Game) Update() error {
 	if inpututil.IsKeyJustPressed(ebiten.KeyF) {
@@ -31,12 +76,28 @@ func (g *Game) Update() error {
 	if inpututil.IsKeyJustPressed(ebiten.KeyEscape) {
 		return errors.New("ahahaha")
 	}
+
+	mouseX, mouseY := ebiten.CursorPosition()
+	activeBlock.x = float32(mouseX) - float32(mouseX%SQUARESIDE)
+	activeBlock.y = float32(mouseY) - float32(mouseY%SQUARESIDE)
+
+	blocksIdx1, blocksIdx2 := blockPos2BlocksIdx(activeBlock)
+
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButton0) {
+		blocks[blocksIdx1][blocksIdx2].bType = BTSAND
+		blocks[blocksIdx1][blocksIdx2].pos.x = activeBlock.x
+		blocks[blocksIdx1][blocksIdx2].pos.y = activeBlock.y
+	}
+
+	if inpututil.IsKeyJustPressed(ebiten.KeyC) {
+		clearBlocks()
+	}
+
 	return nil
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	mouse_x, mouse_y := ebiten.CursorPosition()
-	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %v\nMouse pos: x%v y%v", ebiten.ActualFPS(), mouse_x, mouse_y))
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %v\n", ebiten.ActualFPS()))
 
 	for y := 0; y <= SCREENHEIGHT; y += SQUARESIDE {
 		vector.StrokeLine(screen, 0, float32(y), SCREENWIDTH, float32(y), 1, color.RGBA{20, 20, 20, 10}, true)
@@ -45,7 +106,16 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		vector.StrokeLine(screen, float32(x), 0, float32(x), SCREENHEIGHT, 1, color.RGBA{20, 20, 20, 10}, true)
 	}
 
-	vector.DrawFilledRect(screen, float32(mouse_x)-float32(mouse_x%SQUARESIDE), float32(mouse_y)-float32(mouse_y%SQUARESIDE), SQUARESIDE, SQUARESIDE, color.RGBA{255, 0, 0, 255}, true)
+	for idx1 := range blocks {
+		for _, block := range blocks[idx1] {
+			if block.bType != BTAIR {
+				vector.DrawFilledRect(screen, block.pos.x, block.pos.y, SQUARESIDE, SQUARESIDE, color.RGBA{200, 100, 100, 255}, true)
+			}
+
+		}
+	}
+
+	vector.DrawFilledRect(screen, activeBlock.x, activeBlock.y, SQUARESIDE, SQUARESIDE, color.RGBA{255, 0, 0, 255}, true)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
